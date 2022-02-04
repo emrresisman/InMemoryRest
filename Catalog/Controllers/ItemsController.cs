256f845bd9aef57c9e7eef5f79c419facf065e08
emre.sisman <emre.sisman@AL05ERPES>
2022-02-04 13:53:06 +0300

    Add project files.

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,79 @@
+﻿using Catalog.Dtos;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    //Get /itemsamk
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private IItemsRepository repository;
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select(item => item.AsDto());
+            return items;
+        }
+        // Get / items / {id}
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return item.AsDto();
+        }
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new Item()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+            repository.CreateItem(item);
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+            Item UpdatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+            repository.UpdateItem(UpdatedItem);
+
+            return NoContent();
+        }
+        [HttpDelete]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+            repository.DeleteItem(id);
+            return NoContent();
+        }
+    }
+}
